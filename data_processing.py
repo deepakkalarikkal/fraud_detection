95c4656dd66e975e7f3d99f3d83db17f2add3a11
deepakkalarikkal <deepakkalarikkal@gmail.com>
2022-01-29 19:50:02 +0530

    defined a sum

--- data_processing.py
+++ data_processing.py
@@ -2,4 +2,12 @@
 
 print("dta processing git programming and synch example")
 
-Print("try something without a message")
+print("try something without a message")
+
+def sum(a,b):
+
+    c=a+b
+    return c
+
+m= sum(2,4)
+print("the sum of two numbers is", m)
